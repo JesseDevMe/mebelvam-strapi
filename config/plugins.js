25bf94029fd81bfa64fe4885ad5ca2476053966b
Nick <fomaevil@mail.ru>
2023-11-19 22:02:27 +0300

    save

---
+++ config/plugins.js
@@ -0,0 +1,23 @@
+
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'mebel-vam-next@mail.ru',
+        defaultReplyTo: 'mebel-vam-next@mail.ru',
+      },
+    },
+  },
+  // ...
+});
